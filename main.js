c735454ae9886ff7f644c564d01fbdd1b01d8ac2
Rene Pilz <rene@pilz.cc>
2021-02-12 21:06:29 +0100

    2nd try with compact mode

--- main.js
+++ main.js
@@ -15,65 +15,21 @@ let adapter;
 function startAdapter(options) {
   options = options || {};
   Object.assign(options, {
-       name: 'zoe2'
+       name: 'zoe2',
+       ready: main,
+       unload: callback => {
+	       	try {
+			adapter.log.debug('cleaned everything up...');
+			callback();
+		} catch (e) {
+			callback();
+		}
+       }
+       
   });
 
   adapter = new utils.Adapter(options);
   return adapter;
-});
-
- 
-
-// is called when adapter shuts down - callback has to be called under any
-// circumstances!
-adapter.on('unload', function(callback) {
-	try {
-		adapter.log.debug('cleaned everything up...');
-		callback();
-	} catch (e) {
-		callback();
-	}
-});
-
-// is called if a subscribed object changes
-adapter.on('objectChange', function(id, obj) {
-	// Warning, obj can be null if it was deleted
-	adapter.log.debug('objectChange ' + id + ' ' + JSON.stringify(obj));
-});
-
-// is called if a subscribed state changes
-adapter.on('stateChange', function(id, state) {
-	// Warning, state can be null if it was deleted
-	
-	adapter.log.debug('stateChange ' + id + ' ' + JSON.stringify(state));
-
-	// you can use the ack flag to detect if it is status (true) or command
-	// (false)
-	if (state && !state.ack) {
-		adapter.log.debug('ack is not set!');
-	}
-});
-
-// Some message was sent to adapter instance over message box. Used by email,
-// pushover, text2speech, ...
-adapter.on('message', function(obj) {
-	adapter.log.info('message');
-	if (typeof obj == 'object' && obj.message) {
-		if (obj.command == 'send') {
-			// e.g. send email or pushover or whatever
-			console.log('send command');
-
-			// Send response in callback if required
-			if (obj.callback)
-				adapter.sendTo(obj.from, obj.command, 'Message received', obj.callback);
-		}
-	}
-});
-
-// is called when databases are connected and adapter received configuration.
-// start here!
-adapter.on('ready', function() {
-	main();
 });
 
 function main() {
